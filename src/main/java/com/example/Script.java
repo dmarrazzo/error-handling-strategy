6763cc4acaccdebf8e96fb721ae38d1d40d9bee4
Donato Marrazzo <donato.marrazzo@gmail.com>
2022-01-21 18:10:55 +0100

    automatic rethrow

--- src/main/java/com/example/Script.java
+++ src/main/java/com/example/Script.java
@@ -12,15 +12,22 @@ public class Script {
         kcontext.setVariable("Error", pwihe);
     }
 
+    public static void rethrow(ProcessContext kcontext) {
+        ProcessWorkItemHandlerException pwihe = (ProcessWorkItemHandlerException) kcontext.getVariable("Error");
+        pwihe = new ProcessWorkItemHandlerException(pwihe.getProcessId(), ProcessWorkItemHandlerException.HandlingStrategy.RETHROW, pwihe.getCause());
+        kcontext.setVariable("Error", pwihe);
+        kcontext.setVariable("retry", 0);
+        kcontext.setVariable("RetryOnError", "0");
+    }
+
     public static void decreaseRetry(ProcessContext kcontext) {
         var retryOnError = (String) kcontext.getVariable("RetryOnError");
-        var retryOnErrorInt = Integer.parseInt(retryOnError)-1;
-        kcontext.setVariable("RetryOnError", Integer.toString(retryOnErrorInt));
+        var retryOnErrorInt = Integer.parseInt(retryOnError);
         ProcessWorkItemHandlerException pwihe = (ProcessWorkItemHandlerException) kcontext.getVariable("Error");
-        if (retryOnErrorInt > 0) {
-            pwihe = new ProcessWorkItemHandlerException(pwihe.getProcessId(), ProcessWorkItemHandlerException.HandlingStrategy.RETRY, pwihe.getCause(), retryOnErrorInt);
-            kcontext.setVariable("Error", pwihe);                
-        }
+        pwihe = new ProcessWorkItemHandlerException(pwihe.getProcessId(), ProcessWorkItemHandlerException.HandlingStrategy.RETRY, pwihe.getCause(), retryOnErrorInt);
+        kcontext.setVariable("Error", pwihe);                
+        retryOnErrorInt--;
+        kcontext.setVariable("RetryOnError", Integer.toString(retryOnErrorInt));
         kcontext.setVariable("retry", retryOnErrorInt);
-    }
+   }
 }
